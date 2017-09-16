7345e683d06ee8f9b870a6c5b2d27e622f03da9f
BalaSadaphal <sadaphalbalasaheb@gmail.com>
2017-09-16 18:09:36 +0530

    Update Reducer2.java

--- nordStorm/productRatings/SecondarySort/Reducer2.java
+++ nordStorm/productRatings/SecondarySort/Reducer2.java
@@ -9,10 +9,24 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 public class Reducer2 extends Reducer<TextIntWritablePair, Text, LongWritable, Text> {
 	
 	int count=1;
+	
+	MultipleOutputs<LongWritable, Text> multipleOutputs;
+	
+	@Override
+	protected void setup(
+			Reducer<TextIntWritablePair, Text, LongWritable, Text>.Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		
+		multipleOutputs = new MultipleOutputs<LongWritable, Text>(context);
+		
+	}
+	
 	@Override
 	protected void reduce(TextIntWritablePair key, Iterable<Text> entireLines,
 			Reducer<TextIntWritablePair, Text, LongWritable, Text>.Context context)
@@ -24,11 +38,19 @@ public class Reducer2 extends Reducer<TextIntWritablePair, Text, LongWritable, T
 		for(Text line:entireLines)
 			{
 				if (count <= 10)
-				context.write(new LongWritable(count), line);
+				multipleOutputs.write(new LongWritable(count), line, key.getBrand().toString());	
+				//context.write(new LongWritable(count), line);
 				count++;
 				
 			}
 					
+	}
+	
+	@Override
+	protected void cleanup(
+			Reducer<TextIntWritablePair, Text, LongWritable, Text>.Context context)
+			throws IOException, InterruptedException {
+		multipleOutputs.close();
 	}
 
 }
